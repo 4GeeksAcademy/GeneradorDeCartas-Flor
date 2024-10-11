28719b49646441763b475f8f5c02ad65425002d7
Florencia <florenciamayaumpierrez@gmail.com>
2024-10-11 22:11:02 +0000

    Generador de Cartas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardValues = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "A",
+    "J",
+    "Q",
+    "K"
+  ];
+  let cardPintas = ["♦", "♥", "♠", "♣"];
+
+  function elegirAleatorio(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+  const valorAreatorio = elegirAleatorio(cardValues);
+  document.querySelector(".value").innerHTML = valorAreatorio;
+  const pintaAleatoria = elegirAleatorio(cardPintas);
+  document.querySelectorAll(".pinta").forEach(elementoPinta => {
+    elementoPinta.innerHTML = pintaAleatoria;
+    if (pintaAleatoria == "♥" || pintaAleatoria == "♦") {
+      elementoPinta.style.color = "red";
+    }
+  });
 };
